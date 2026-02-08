ae04571b637e28feb51f13eec69c0541a4ebd73c
Matteo Angelini <angelini.matteo27@gmail.com>
2026-02-08 16:57:22 +0100

    First commit

---
+++ src/main/java/com/pratichiera/pratichieraga/PratichieraGaApplication.java
@@ -0,0 +1,13 @@
+package com.pratichiera.pratichieraga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PratichieraGaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PratichieraGaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pratichiera/pratichieraga/PratichieraGaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratichiera.pratichieraga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PratichieraGaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
