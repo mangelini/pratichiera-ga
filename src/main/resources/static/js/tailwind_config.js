fe6dd3a36b5d0005c4e63d426bb21029a610a4a2
Matteo Angelini <angelini.matteo27@gmail.com>
2026-02-14 16:05:39 +0100

    v0.2

---
+++ src/main/resources/static/js/tailwind_config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#73212B',
+                    hover: '#1D1D1D',
+                    focus: 'rgba(115, 33, 43, 0.25)',
+                },
+            },
+        },
+    },
+}
